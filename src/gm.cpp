6d6a51e628cfdbabb3c6ede26e406a1ba7e78534
Ali Hanafi <alihanafi.code.email@gmail.com>
2023-01-06 14:32:17 +0330

    add some git commands

---
+++ src/gm.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <span>
+#include <string>
+using std::cout;
+using std::span;
+using std::string;
+using std::system;
+
+int main(int argc, char* argv[]) noexcept
+{
+    constexpr int minArgLimit = 2;
+
+    if (argc < minArgLimit) {
+        cout << "branch should be pass as argument\n";
+        return EXIT_FAILURE;
+    }
+
+    auto args = span(argv, static_cast<size_t>(argc));
+    const string command = string("git merge ") + args[1];
+    system(command.data()); // NOLINT
+}
